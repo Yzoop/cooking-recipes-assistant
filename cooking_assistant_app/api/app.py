b26979823d7a9783d9f724d46b1a16dd812313f7
Ivan Kalinchuk <iv.ikalin@gmail.com>
2024-12-30 13:04:50 +0100

    added authentication

--- cooking_assistant_app/api/app.py
+++ cooking_assistant_app/api/app.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI, HTTPException
+import os
+
+from dotenv import load_dotenv
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from pydantic import HttpUrl
 
 from cooking_assistant_app.utils.prompt_manager import OpenaiApiManager
 
 app = FastAPI()
+security = HTTPBearer()
+load_dotenv()
 
-# Initialize OpenAI API Manager
 api_manager = OpenaiApiManager()
+
+# Simulated token for demo purposes
+API_TOKEN = os.environ.get("ASSISTANT_API_KEY")
+print(API_TOKEN)
+
+
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    if credentials.credentials != API_TOKEN:
+        raise HTTPException(status_code=401, detail="Invalid or missing authentication token")
 
 
 @app.get("/")
@@ -14,7 +28,7 @@ def home():
     return {"message": "Welcome to the Recipe Parsing API!"}
 
 
-@app.post("/parse-recipe/")
+@app.get("/parse-recipe/", dependencies=[Depends(verify_token)])
 def parse_recipe(website_url: HttpUrl):
     try:
         recipe = api_manager.get_recipe(website_url=website_url)
