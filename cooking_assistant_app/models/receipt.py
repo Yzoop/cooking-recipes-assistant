907254116e2a5c8de6712225ff9a30c141e80c76
Ivan Kalinchuk <iv.ikalin@gmail.com>
2024-12-30 13:05:17 +0100

    removed null response in the receipt ingridents - pointless to have unknown ingredient

--- cooking_assistant_app/models/receipt.py
+++ cooking_assistant_app/models/receipt.py
@@ -15,20 +15,21 @@ class Unit(StrEnum):
 
 class Ingredient(BaseModel):
     name: str = Field(..., description="Name of the ingredient.")
-    quantity: float | None = Field(None, description="Quantity of the ingredient.")
-    unit: Unit | None = Field(None, description="Unit of measurement (e.g., piece, ml, g).")
+    quantity: float = Field(..., description="Quantity of the ingredient.")
+    unit: Unit = Field(..., description="Unit of measurement (e.g., piece, ml, g).")
 
 
 class DishInfo(BaseModel):
-    calories: int | None = Field(None, description="Calories per serving.")
-    prep_time: int | None = Field(None, description="Preparation time in minutes.")
-    cook_time: int | None = Field(None, description="Cooking time in minutes.")
-    total_time: int | None = Field(None, description="Total time in minutes (auto-calculated).")
+    calories: int = Field(..., description="Calories per serving.")
+    prep_time: int = Field(..., description="Preparation time in minutes.")
+    cook_time: int = Field(..., description="Cooking time in minutes.")
+    total_time: int = Field(..., description="Total time in minutes (auto-calculated).")
 
 
 class Recipe(BaseModel):
     title: str = Field(..., description="Title of the recipe.")
     ingredients: list[Ingredient] = Field(..., description="List of ingredients.")
+    # TODO: add pictures to the steps, make the more structured
     steps: List[str] = Field(..., description="Step-by-step instructions.")
-    dish_info: DishInfo | None = Field(None, description="Additional information about the dish.")
-    tags: List[str] | None = Field(None, description="Tags for categorizing the recipe.")
+    dish_info: DishInfo = Field(..., description="Additional information about the dish.")
+    tags: List[str] = Field(..., description="Tags for categorizing the recipe.")
