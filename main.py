cd01324e44b095c39f20a5b14a7e202b9abb24dd
Ivan Kalinchuk <iv.ikalin@gmail.com>
2024-12-30 12:04:04 +0100

    added prompt manager, try out on a pizza-website

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from pathlib import Path
+
+from utils.prompt_manager import OpenaiApiManager
+
 if __name__ == "__main__":
-    print(123)
+    prompt_manager = OpenaiApiManager(prompt_path=Path("./openai_prompts/prompt_parse_url_recipe"))
+    print(
+        prompt_manager.get_recipe(
+            website_url="https://www.indianhealthyrecipes.com/pizza-recipe-make-pizza/"
+        )
+    )
---
+++ utils/prompt_manager.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path
+
+import instructor
+from dotenv import load_dotenv
+from openai import OpenAI
+from urllib3.util import url
+
+from models.receipt import Recipe
+from utils.web_utils import get_website_context
+
+# TODO: add feature: replace ingredient / what you have or recommended
+# TODO: add recommendations how to modify the receipt
+# TODO: add tools what people have, like oven, air-fryer etc.
+
+
+class OpenaiApiManager:
+    def __init__(
+        self,
+        prompt_path: Path,
+        openai_api_key_name: str = "OPENAI_API_KEY",
+    ):
+        if prompt_path.exists():
+            with open(prompt_path) as prompt_file:
+                self.__prompt = prompt_file.read()
+        else:
+            raise FileNotFoundError(f"Prompt {prompt_path} does not exist!")
+        if "{website_context}" not in self.__prompt:
+            raise ValueError("Given prompt does not contain placeholder for the website url!")
+
+        load_dotenv()
+        self.__openai_client = instructor.from_openai(
+            OpenAI(api_key=os.getenv(openai_api_key_name))
+        )
+
+    def get_recipe(self, website_url: url, gpt_model: str = "gpt-4o-mini") -> Recipe:
+        # Extract structured data from natural language
+        processed_prompt = self.__process_prompt(website_url)
+        recipe = self.__openai_client.chat.completions.create(
+            model="gpt-4o-mini",
+            response_model=Recipe,
+            messages=[{"role": "user", "content": processed_prompt}],
+        )
+        return recipe
+
+    def __process_prompt(self, website_url: url):
+        page_content = get_website_context(website_url)
+        return self.__prompt.format(website_context=page_content)
