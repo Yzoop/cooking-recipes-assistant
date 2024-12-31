106777b2f2a7b4674a1fa9cbe816c60cecdef3f9
Ivan Kalinchuk <iv.ikalin@gmail.com>
2024-12-31 11:39:44 +0100

    renamed try out script

--- cooking_assistant/try_out.py
+++ cooking_assistant/try_out.py
@@ -1,9 +1,11 @@
+from pydantic import HttpUrl
+
 from cooking_assistant.utils.prompt_manager import OpenaiApiManager
 
 if __name__ == "__main__":
     prompt_manager = OpenaiApiManager()
     print(
         prompt_manager.get_recipe(
-            website_url="https://www.indianhealthyrecipes.com/pizza-recipe-make-pizza/"
+            website_url=HttpUrl("https://www.indianhealthyrecipes.com/pizza-recipe-make-pizza/")
         )
     )
