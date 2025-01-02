253a4efbe96b68d318a17cf640b4fd01fcccb0fd
Ivan Kalinchuk <iv.ikalin@gmail.com>
2025-01-02 13:54:17 +0100

    trying out the tiktok code

--- cooking_assistant/try_out.py
+++ cooking_assistant/try_out.py
@@ -1,18 +1,16 @@
 from pydantic import HttpUrl
 
-from cooking_assistant.utils.web_utils import TikTokManager
+from cooking_assistant.utils.prompt_manager import OpenaiApiManager
+
+# from cooking_assistant.utils.web_utils import TikTokManager
 
 if __name__ == "__main__":
-    # prompt_manager = OpenaiApiManager()
-    # print(
-    #     prompt_manager.get_recipe(
-    #         website_url=HttpUrl("https://www.indianhealthyrecipes.com/pizza-recipe-make-pizza/")
+    prompt_manager = OpenaiApiManager()
+    print(prompt_manager.get_recipe(recipe_url=HttpUrl("https://vm.tiktok.com/ZMkAKXcth/")))
+    # tt_manager = TikTokManager()
+    # tt_manager.get_tiktok_captions(
+    #     tt_video_url=HttpUrl(
+    #         "https://www.tiktok.com/@martellifoods/video/7124401218184596742"
+    #         "?is_from_webapp=1&sender_device=pc&web_id=7383700479904171552"
     #     )
     # )
-    tt_manager = TikTokManager()
-    tt_manager.get_tiktok_captions(
-        tt_video_url=HttpUrl(
-            "https://www.tiktok.com/@martellifoods/video/7124401218184596742"
-            "?is_from_webapp=1&sender_device=pc&web_id=7383700479904171552"
-        )
-    )
