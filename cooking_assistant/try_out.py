6ed20c874ca6e8ddcdfbd72e866935a7450144a6
Ivan Kalinchuk <iv.ikalin@gmail.com>
2025-01-06 02:04:43 +0100

    update try out

--- cooking_assistant/try_out.py
+++ cooking_assistant/try_out.py
@@ -9,17 +9,17 @@
 # from cooking_assistant.recipe.recipe_manager import RecipeManager
 #
 # if __name__ == "__main__":
-#     # recipe_manager = RecipeManager()
-#     # res = asyncio.run(
-#     #     recipe_manager.create_recipe(
-#     #         language=Language.UKRAINIAN,
-#     #         user_id="user_a",
-#     #         recipe_url=HttpUrl("https://vm.tiktok.com/ZMkAKXcth/"),
-#     #     )
-#     # )
-#     # print(res)
-#     db_manager = RecipeDbManager()
-#     print(db_manager.get_recipe_ids(user_id="user_a"))
+#     recipe_manager = RecipeManager()
+#     res = asyncio.run(
+#         recipe_manager.create_recipe(
+#             language=Language.ENGLISH,
+#             user_id="3Gl9L5VDwIMZ4OZBLLnlH5JLnI12",
+#             recipe_url=HttpUrl("https://www.tiktok.com/@boredoflunch/video/7330668395534241057?lang=en"),
+#         )
+#     )
+#     print(res)
+#     # db_manager = RecipeDbManager()
+#     # print(db_manager.get_recipe_ids(user_id="user_a"))
 #     # img_gen = ImageGenerator()
 #     # res = asyncio.run(img_gen.generate_image("tasty fish", return_type="base64_str"))
 #     # print(res)
